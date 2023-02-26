2577e610104f1c78c91bbb7610899d330b6bdc0d
Bhavicka Mohta <91835014+vicka1228@users.noreply.github.com>
2023-02-26 22:15:12 +0400

    boilerplate code

---
+++ client.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include <netinet/in.h>
+
+#include<unistd.h>
+#include<stdlib.h>
+
+int main()
+{
+	//socket
+	int server_sd = socket(AF_INET,SOCK_STREAM,0);
+	if(server_sd<0)
+	{
+		perror("socket:");
+		exit(-1);
+	}
+	//setsock
+	int value  = 1;
+	setsockopt(server_sd,SOL_SOCKET,SO_REUSEADDR,&value,sizeof(value)); //&(int){1},sizeof(int)
+	struct sockaddr_in server_addr;
+	bzero(&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(5000);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //INADDR_ANY, INADDR_LOOP
+
+	//connect
+    if(connect(server_sd,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+    {
+        perror("connect");
+        exit(-1);
+    }
+	
+	//accept
+	char buffer[256];
+
+	while(1)
+	{
+       fgets(buffer,sizeof(buffer),stdin);
+       buffer[strcspn(buffer, "\n")] = 0;  //remove trailing newline char from buffer, fgets does not remove it
+       if(strcmp(buffer,"bye")==0)
+        {
+        	printf("closing the connection to server \n");
+        	close(server_sd);
+            break;
+        }
+        if(send(server_sd,buffer,strlen(buffer),0)<0)
+        {
+            perror("send");
+            exit(-1);
+        }
+
+		int bytes = recv(server_sd,buffer,sizeof(buffer),0);
+
+		printf("%s\n", buffer);
+
+        bzero(buffer,sizeof(buffer));			
+	}
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,118 @@
+//============================================================================
+// Name         : Chat Server using Select()
+// Description  : This Program will receive messages from several clients using
+//                select() system class
+//============================================================================
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<sys/select.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main()
+{
+	//socket
+	int server_sd = socket(AF_INET,SOCK_STREAM,0);
+	printf("Server fd = %d \n",server_sd);
+	if(server_sd<0)
+	{
+		perror("socket:");
+		exit(-1);
+	}
+	//setsock
+	int value  = 1;
+	setsockopt(server_sd,SOL_SOCKET,SO_REUSEADDR,&value,sizeof(value)); //&(int){1},sizeof(int)
+	struct sockaddr_in server_addr;
+	bzero(&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(5000);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //INADDR_ANY, INADDR_LOOP
+
+	//bind
+	if(bind(server_sd, (struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+	{
+		perror("bind failed");
+		exit(-1);
+	}
+	//listen
+	if(listen(server_sd,5)<0)
+	{
+		perror("listen failed");
+		close(server_sd);
+		exit(-1);
+	}
+	
+	fd_set full_fdset;
+	fd_set read_fdset;
+	FD_ZERO(&full_fdset);
+
+	int max_fd = server_sd;
+
+	FD_SET(server_sd,&full_fdset);
+
+	printf("Server is listening...\n");
+	while(1)
+	{
+		printf("max fd = %d \n",max_fd);
+		read_fdset = full_fdset;
+
+		if(select(max_fd+1,&read_fdset,NULL,NULL,NULL)<0)
+		{
+			perror("select");
+			exit (-1);
+		}
+
+		for(int fd = 3 ; fd<=max_fd; fd++)
+		{
+			if(FD_ISSET(fd,&read_fdset))
+			{
+				if(fd==server_sd)
+				{
+					int client_sd = accept(server_sd,0,0);
+					printf("Client Connected fd = %d \n",client_sd);
+					FD_SET(client_sd,&full_fdset);
+					
+					if(client_sd>max_fd)	
+						max_fd = client_sd;
+				}
+				else
+				{
+					char buffer[256];
+					bzero(buffer,sizeof(buffer));
+					int bytes = recv(fd,buffer,sizeof(buffer),0);
+					if(bytes==0)   //client has closed the connection
+					{
+						printf("connection closed from client side \n");
+						close(fd);
+						FD_CLR(fd,&full_fdset);
+						if(fd==max_fd)
+						{
+							for(int i=max_fd; i>=3; i--)
+								if(FD_ISSET(i,&full_fdset))
+								{
+									max_fd =  i;
+									break;
+								}
+						}
+					}
+
+					if(send(fd,buffer,strlen(buffer),0)<0)
+			        {
+			            perror("send");
+			            exit(-1);
+			        }
+
+					printf("%s \n",buffer);
+				}
+			}
+		}
+
+	}
+
+	//close
+	close(server_sd);
+	return 0;
+}
